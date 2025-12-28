9cf79030e20ecf381ed5da457be6fe0ddc3ff505
empiricalcode <daniellongschool@gmail.com>
2025-12-28 17:34:58 -0500

    feat: add hash to job dataclass

--- src/models/job.py
+++ src/models/job.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import hashlib
 
 
 @dataclass
@@ -12,3 +13,9 @@ class Job:
     def __str__(self) -> str:
         locations_str = ", ".join(self.location)
         return f"{self.title} ({self.department}) - {locations_str} - {self.work_arrangement}"
+
+    def hash(self) -> str:
+        title_part = self.title.lower().replace(" ", "")
+        location_part = ",".join(sorted([loc.lower().replace(" ", "") for loc in self.location]))
+        hash_input = title_part + location_part
+        return hashlib.sha256(hash_input.encode()).hexdigest()
